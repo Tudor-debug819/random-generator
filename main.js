c8927f5b0e12f58ab95f52bf0e796e3160485832
Tudor-debug819 <tovidiua@emeal.nttdata.com>
2024-10-02 23:38:11 +0300

    task done

---
+++ main.js
@@ -0,0 +1,25 @@
+document
+  .getElementById("generateButton")
+  .addEventListener("click", function () {
+    const nameInput = document.getElementById("nameInput").value;
+
+    const names = nameInput
+      .split(/[\s,]+/)
+      .filter((name) => name.trim() !== "");
+
+    if (names.length < 3) {
+      alert("Please enter at least 3 names.");
+      return;
+    }
+
+    const randomIndex = Math.floor(Math.random() * names.length);
+
+    document.getElementById(
+      "randomName"
+    ).textContent = `The winner that has to pay the bill is: ${names[randomIndex]}`;
+  });
+
+document.getElementById("refreshButton").addEventListener("click", function () {
+  document.getElementById("nameInput").value = "";
+  document.getElementById("randomName").textContent = "";
+});
